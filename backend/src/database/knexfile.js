f1adc8cc19b8b7dbdf542f22df6e93de10432915
PatrickWesley7 <pwesley1@gmail.com>
2026-07-10 23:16:50 -0300

    feat: Configure database with Knex

---
+++ backend/src/database/knexfile.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'senha123',
+      database: process.env.DB_NAME || 'restaurant_pos'
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    }
+  }
+};
